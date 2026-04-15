a322d46e3671f17ff68c7504cf58ddc0c3fe398a
Mamun-Hossain-Dev <mh4559641@gmail.com>
2026-04-15 15:04:02 +0600

    added the prisma feature

--- src/modules/banner/banner.repository.ts
+++ src/modules/banner/banner.repository.ts
@@ -2,5 +2,11 @@ import { Prisma } from '@prisma/client'
 import prisma from '../../lib/prisma'
 
 const create = async (data: Prisma.BannerCreateInput) => {
-  return prisma.banner.create
+  return prisma.banner.create({
+    data,
+  })
+}
+
+export const bannerRepository = {
+  create,
 }
